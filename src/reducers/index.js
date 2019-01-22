24039fe02d721aa9ec57b4f79e293ee0ed0ba8c9
Anne Chepkeitany <rerimoianne.4@gmail.com>
2019-01-22 10:16:44 +0300

    Subscribe to updates in Redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,6 +3,9 @@ export default (state, action) => {
     switch (action.type) {
         case "SET_TECHNOLOGY":
             // Update the technology
+            // Use the ES6 spread operator ...
+            // However, the tech field is updated
+            // with what comes from action.text
             return {
                 ...state,
                 tech: action.text
