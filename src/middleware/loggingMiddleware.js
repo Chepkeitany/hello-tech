f7342a524225e838f7000094cc2ccdb959f09554
Anne Chepkeitany <rerimoianne.4@gmail.com>
2019-02-06 09:11:23 +0300

    Build a custom middleware for logging

--- src/middleware/loggingMiddleware.js
+++ src/middleware/loggingMiddleware.js
@@ -1,23 +1,18 @@
 const loggingMiddleware = ({ getState, dispatch }) => next => action => {
     // pass action on to the next middleware/reducers
+    console.log("%c state upon reaching middleware 👉 ", "background:purple;color:white", getState());
     next(action);
 
     // conditional check
     // further dispatch calls are exited here    
-    if (action.type !== "LOGGER") {
-        return;
-    }
+    // if (action.type !== "LOGGER") {
+    //     return;
+    // }
     // // Get DNA to know the order of parameters
     // console.log("%c Action 👉 ", "background:yellow;color:black", action);
-    console.log("%c getState 👉 ", "background:purple;color:white", getState());
+    console.log("%c current stat of the app 👉 ", "background:yellow;color:black", getState());
     // console.log("%c dispatch 👉 ", "background:green;color:white", dispatch);
     // console.log("%c next 👉 ", "background:teal;color:white", next);
-
-    // dispatch call
-    dispatch({
-        type: "TEST_DISPATCH",
-        payload: {}
-    })
 };
 
 export default loggingMiddleware;
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,8 @@
 import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
 import reducer from "../reducers";
 import loggingMiddleware from "../middleware/loggingMiddleware";
 
 const initialState = { tech: "React "};
-const store = createStore(reducer, initialState, applyMiddleware(loggingMiddleware));
+const store = createStore(reducer, initialState, applyMiddleware(loggingMiddleware, logger));
 export default store;
